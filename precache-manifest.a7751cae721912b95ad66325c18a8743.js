ff95082a9479dfd4afc7614161bfc593e9c2a7f6
Russel Rodrigues <russel.rodrigues@sts.in>
2020-05-05 19:25:20 +0530

    Updates

---
+++ precache-manifest.a7751cae721912b95ad66325c18a8743.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa8d9f7875a5a4e21c1e",
+    "url": "/jude/static/css/main.f01bbda3.chunk.css"
+  },
+  {
+    "revision": "aa8d9f7875a5a4e21c1e",
+    "url": "/jude/static/js/main.aa8d9f78.chunk.js"
+  },
+  {
+    "revision": "fc68e841da238c75adc4",
+    "url": "/jude/static/js/1.fc68e841.chunk.js"
+  },
+  {
+    "revision": "93a7c1404ea37fee4701",
+    "url": "/jude/static/js/runtime~main.93a7c140.js"
+  },
+  {
+    "revision": "f8220c56e26bb2ccf213f74fc34c5382",
+    "url": "/jude/index.html"
+  }
+];
